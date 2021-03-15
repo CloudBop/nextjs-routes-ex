101f63dcb489214579933bc0d8287778d92c381f
Cloudbop <boppincolin@gmail.com>
2021-03-15 02:32:02 +0000

    added links for clients + dynamic routes

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,26 @@
 import Link from 'next/link'
+import {useRouter} from 'next/router'
 function ClientProjectPage() {
+  const router = useRouter()
+
+  function loadProjectHandler() {
+
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'projecta' },
+    });
+
+    // router.replace -> stop client from going back
+  }
+
   return (
     <div>
-      <h1>The client projects page.</h1>
-      <p>back to <Link href={"/"}><a>home</a></Link></p>
+      <h1>The specific client projects page.</h1>
+
+      <p>back to <Link href={"/clients"}><a>Clients</a></Link></p>
+
+      <button onClick={loadProjectHandler}>Load Project</button>
     </div>
   )
 }
